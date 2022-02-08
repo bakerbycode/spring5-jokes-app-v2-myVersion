46dada1bdf4ae3e28c4633cf1222b3c96b835ca6
Sena Uenal <sena.uenal@awacademyconsultant.de>
2022-02-08 18:24:21 +0100

    Use Spring Initializr to create a new Spring Boot Project. Closes #1

---
+++ src/test/java/com/example/spring5jokesappv2myversion/Spring5JokesAppV2MyVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring5jokesappv2myversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV2MyVersionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
