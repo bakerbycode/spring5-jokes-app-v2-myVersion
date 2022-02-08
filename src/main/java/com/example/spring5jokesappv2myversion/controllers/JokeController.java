5bd3703598d7d803bca4d3a80eb3a787c508252d
Sena Uenal <sena.uenal@awacademyconsultant.de>
2022-02-08 18:59:00 +0100

    Add JokeController. closes #4.
    - autoinject jokeService
    - create method for GET request of root context "/" & ""
    - return index view

---
+++ src/main/java/com/example/spring5jokesappv2myversion/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package com.example.spring5jokesappv2myversion.controllers;
+
+import com.example.spring5jokesappv2myversion.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService){
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping({"/",""}) //Alte Schreibweise: @RequestMapping
+    public String showJoke(Model model){
+    model.addAttribute(jokeService.getChuckNorrisJoke());
+
+        return "index";
+    }
+}
